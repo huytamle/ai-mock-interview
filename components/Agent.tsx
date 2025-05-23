23a919c35452699b9eb734de1d67ed3e252c3074
huytamlee <huytamlee@gmail.com>
2025-05-23 10:38:52 +0700

    Ignore Eslint and Typescript errors (for now)

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -48,7 +48,13 @@ const Agent = ({ userName }: AgentProps) => {
       {messages.length > 0 && (
         <div className="transcript-border">
           <div className="transcript">
-            <p key={lastMessage} className="">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
               {lastMessage}
             </p>
           </div>
